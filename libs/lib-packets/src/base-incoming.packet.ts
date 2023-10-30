1795c68dd21a00a4346128dcea5318ce52254318
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-10-29 20:36:04 -0400

    cleanup

--- libs/lib-packets/src/base-incoming.packet.ts
+++
@@ -1,7 +0,0 @@
-import {MessagingInternalEvent} from '@envy/lib-client';
-
-export interface IncomingPacketBase<D> {
-  buffer: Buffer;
-  constructor(data: D): Buffer;
-  header: MessagingInternalEvent;
-}
